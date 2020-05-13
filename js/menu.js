0555a1f92f61ee1d37f645eaf652dd7d2fa672ec
Patrick Dunn <alan.burt@mackillop.act.edu.au>
2020-05-13 14:52:46 +1000

    re

--- js/menu.js
+++ js/menu.js
@@ -2,12 +2,13 @@ var menuState = {
   create: function() {
     game.add.image(0, 0, 'menubg');
     button = game.add.button(game.world.centerX, game.world.centerY, 'button');
+    button.anchor.setTo(0.5, 0.5);
     button.onInputUp.add(this.startGame);
     var text = game.add.text(button.x, button.y, 'Start Game');
     text.anchor.setTo(0.5, 0.5);
   },
 
-  startGane: function() {
+  startGame: function() {
     game.state.start("level1")
   },
 };
