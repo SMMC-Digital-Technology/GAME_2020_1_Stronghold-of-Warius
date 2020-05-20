b512aea2d4ecbc9d50fe56e180d905efd515a6e9
Tom Rodgers <alan.burt@mackillop.act.edu.au>
2020-05-20 12:17:22 +1000

    Update level1.js

--- js/level1.js
+++ js/level1.js
@@ -31,22 +31,34 @@ var level1State = {
      slime1 = game.add.sprite(400, 500, "slimeg");
      game.physics.arcade.enable(slime1);
      slime1.collideWorldBounds = true;
-     slime1.animations.add('left', [0, 1, 2], 0, true);
+     slime1.animations.add('slime wobble', [0, 1, 2], 0, true);
 
      //health bar
-     healthBar = game.add.image(325, 25, "healthbar");
+     healthBar = game.add.image(250, 25, "healthbar");
      healthBar.anchor.setTo(1, 0);
-     healthBar.width = game.global.health / game.global.healthM * 300;
+     healthBar.width = game.global.health / game.global.healthM * 200;
      healthBar.fixedToCamera = true;
      //mana bar
-     manaBar = game.add.image(325, 50, "manabar");
+     manaBar = game.add.image(400, 200, "manabar");
      manaBar.anchor.setTo(1, 0);
-     manaBar.width = game.global.mana / game.global.manaM * 300;
+     manaBar.width = game.global.mana / game.global.manaM * 200;
      manaBar.fixedToCamera = true;
    },
 
    update: function() {
    this.movePlayer();
+
+   var distance = player.x - slime1.x;
+     if (distance < 0 && distance > -400 && slime1.x > 10) {
+       slime1.body.velocity.x = -120;
+       slime1.animations.play("left");
+     } else if (distance > 0 && distance < 400 && slime1.x < game.world.width) {
+       slime1.body.velocity.x = 120;
+       slime1.animations.play("right");
+     } else {
+       slime1.body.velocity.x = 0;
+     }
+
   },
 
   // moves the player with the cursors
