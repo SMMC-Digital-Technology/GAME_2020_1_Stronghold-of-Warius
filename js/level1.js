b0ec996ddca1d9c04ad37959d6491b4221b9f6b1
Jacob Loughlin <alan.burt@mackillop.act.edu.au>
2020-05-23 18:54:40 +1000

    Added animations and a lot of otehr stuff

--- js/level1.js
+++ js/level1.js
@@ -8,21 +8,24 @@ var level1State = {
     game.add.image(1600, 0, "sewerbg");
     game.add.image(2400, 0, "sewerbg");
     game.add.image(3200, 0, "sewerbg");
-
+    //create player
     player = game.add.sprite(50, 550, "jendolfson");
     game.physics.arcade.enable(player);
     player.body.gravity.y = 9000;
     player.body.collideWorldBounds = true;
+    player.body.setSize(40, 64, 0.5, 0.5);
     //animate the player
-    player.animations.add('right', [0, 1, 2, 3], 10, true);
-    player.animations.add('left', [4, 5, 6, 7], 10, true);
+    player.animations.add('right', [0, 1, 2, 3], 10, false);
+    player.animations.add('left', [4, 5, 6, 7], 10, false);
     player.animations.add('rightSwing', [5, 6, 7, 8], 10, true);
+    player.animations.add('hit', [29, 0], 8, false);
 
     //spawn slime 1
-    slime1 = game.add.sprite(100, 200, "slimeg");
+    slime1 = game.add.sprite(400, 200, "slimeg");
     game.physics.arcade.enable(slime1);
     slime1.body.gravity.y = 9000;
     slime1.body.collideWorldBounds = true;
+    slime1.body.setSize(20, 32, 0.5, 0.5);
     slime1.animations.add('left', [0, 1, 2], 3, true);
     slime1.animations.add('right', [3, 4, 5], 3, true);
     // create keys
@@ -35,22 +38,25 @@ var level1State = {
     // the player, except if the view would go outside the game world
     game.camera.follow(player);
 
-    //health bar
+    //create health bar
     healthBar = game.add.image(325, 25, "healthbar");
     healthBar.anchor.setTo(1, 0);
     healthBar.width = game.global.health / game.global.healthM * 300;
     healthBar.fixedToCamera = true;
-    //mana bar
+    //create mana bar
     manaBar = game.add.image(325, 50, "manabar");
     manaBar.anchor.setTo(1, 0);
     manaBar.width = game.global.mana / game.global.manaM * 300;
     manaBar.fixedToCamera = true;
-
+    //create spell select
     spellselect = game.add.image(650, 25, "spellselect");
     spellselect.fixedToCamera = true;
   },
 
   update: function() {
+    //physics checks
+    game.physics.arcade.overlap(player, slime1, this.hitslime);
+
     this.movePlayer();
 
     var distance = player.x - slime1.x;
@@ -85,9 +91,27 @@ var level1State = {
       player.animations.play('right')
     } else {
       player.body.velocity.x = 0;
-      player.animations.stop();
-      player.frame = 0;
     }
 
+  },
+  removehealth: function(lives) {
+    game.global.health -= lives;
+    if (game.global.health <= 0) {
+      game.state.start("gameover");
+    } else {
+      healthBar.width = game.global.health / game.global.healthM * 300;
+      player.animations.play("hit");
+    }
+  },
+
+  hitslime: function(){
+    if (player.body.touching.right) {
+      player.x -= 30;
+    } else if (player.body.touching.left) {
+      player.x += 30;
+    } else {
+      player.body.velocity.y = -350;
+    }
+    level1State.removehealth(1)
   }
 };
