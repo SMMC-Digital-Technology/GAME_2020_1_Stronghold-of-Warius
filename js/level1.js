7a41eaf6d8d9594b34e629da87215ae67e0c7fd2
Tom Rodgers <alan.burt@mackillop.act.edu.au>
2020-05-26 17:06:02 +1000

    Revert "Update level1.js"
    This reverts commit 17fe5bf064f8fe90e9ffe682b227fb908b754fa1.

--- js/level1.js
+++ js/level1.js
@@ -65,11 +65,12 @@ var level1State = {
     spellselect.fixedToCamera = true;
 
     //create mage bolt
-    bolt = game.add.weapon(10, 'magebolt');
-    bolt.bulletKillType = Phaser.weapon.KILL_WORLD_BOUNDS; //destroyed when off-screen
-    bolt.bulletSpeed = 520; //pixels per second
-    bolt.fireRate = 830; //delay in milliseconds
-    bolt.trackSprite(jendolfson, true);
+    weapon = game.add.weapon(10, 'magebolt');
+    weapon.bulletKillType = Phaser.weapon.KILL_WORLD_BOUNDS; //destroyed when off-screen
+    weapon.bulletKillType = Phaser.weapon.KILL_DISTANCE; //destroyed after a given distance
+    weapon.bulletSpeed = 520; //pixels per second
+    weapon.fireRate = 830; //delay in milliseconds
+    weapon.trackSprite(jendolfson);
   },
 
   update: function(direction, invincible) {
